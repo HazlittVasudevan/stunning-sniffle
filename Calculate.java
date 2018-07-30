f2f90ccd6149036e824dd3886e179d7f904670b3
HazlittVasudevan <41912782+HazlittVasudevan@users.noreply.github.com>
2018-07-30 23:10:27 +0530

    Calculate.java

---
+++ Calculate.java
@@ -0,0 +1,14 @@
+class Calculate
+{
+int num1=23,num2=45;
+public void result()
+{
+num= num1/num2;
+System.out.println(num);
+}
+public static void main(String args[])
+{
+Calculate obj =new Calculate()
+obj.result();
+}
+}
